55f2b633a3d75400a1f4146cc4016e29e2a452cd
柯泰年 <tony.ko@ehsn.com.tw>
2018-10-01 10:20:41 +0800

    update genBanner.py and README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Banner Generator  #
+ 
+### How To Use
+
+1.在size.csv裡頭輸入Banner的寬,高  
+2.執行genBanner.py  
+3.如果圖片不足50張，會自動到http://wallpaperswide.com下載桌布  
+4.處理完畢的Banner會存放於Banner(widthxheight)的資料夾裡  
--- genBanner.py
+++ genBanner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
-from os import mkdir, chdir
+from os import mkdir
 from os.path import exists,join,basename
 from bs4 import BeautifulSoup
 from time import sleep
@@ -15,13 +15,13 @@ def visit_page(url):
     }
     r = requests.get(url, headers = headers)
     r.encoding = 'utf-8'
-    return BeautifulSoup(r.text, 'lxml')
+    return BeautifulSoup(r.text, 'html.parser')
 
 def get_paper_link(page):
     links = page.select('#content > div > ul > li > div > div a')
     return [link.get('href') for link in links]
 
-def download_wallpaper(link, index, total):
+def download_wallpaper(base_dir,link, index, total):
     page_domain = 'http://wallpaperswide.com'
     wallpaper_source = visit_page(page_domain + link)
     wallpaper_size_links = wallpaper_source.select('#wallpaper-resolutions > a')
@@ -36,44 +36,43 @@ def download_wallpaper(link, index, total):
     result = requests.get(page_domain + biggest_one['url'])
 
     if result.status_code == 200:
-        open(biggest_one['name'], 'wb').write(result.content)
+        open(base_dir+biggest_one['name'], 'wb').write(result.content)
 
     if index + 1 == total:
         print('Download completed!\n')
 
-def start(page_start,page_end):
+def genWallpapper(num):
+    if num>=50:
+        return
+    base_dir = './wallpapers/'
+    if not exists(base_dir):
+        mkdir(base_dir)  # /wallpapers
+    page_start = random.randint(2, 6599)
     page_url = 'http://wallpaperswide.com/page/'
-    while page_start <= page_end:
+    while num<50:
         print('\nPreparing to download from the {} page of all the  wallpapers...'.format(page_start))
         PAGE_SOURCE = visit_page(page_url + str(page_start))
         WALLPAPER_LINKS = get_paper_link(PAGE_SOURCE)
         page_start = page_start + 1
 
         for index, link in enumerate(WALLPAPER_LINKS):
-            download_wallpaper(link, index, len(WALLPAPER_LINKS),)
+            download_wallpaper(base_dir,link, index, len(WALLPAPER_LINKS))
             sleep(1)
+        num+=11
 
-def genWallpapper():
-    base_dir = 'wallpapers'
-    if not exists(base_dir):
-        mkdir(base_dir)  # /wallpapers
-    chdir(base_dir)
-    page_start = random.randint(2, 6599)
-    page_end = page_start + 2
-    start(page_start, page_end)
-
-def genBanner():
-    imglist=glob('.\\wallpapers\\*.jpg')
+def genBanner(imglist):
     out_str="Banner"
     with open('size.csv', 'r') as csvfile:
         rows = csv.reader(csvfile, delimiter=',')
         for row in rows:
             out_dir = out_str + "(" + "x".join(row) + ")"
             size=(int(row[0]),int(row[1]))
+            print("Start to resize the image to "+"x".join(row)+"Size Banner")
             for img in imglist:
                 if not exists(out_dir):
                     mkdir(out_dir)
                 resizeBanner(img,out_dir,size)
+            print("Successfully get the ", size, "size Banner")
 
 def resizeBanner(jpgfile, outdir,size=(128,128)):
     img = Image.open(jpgfile)
@@ -84,7 +83,7 @@ def resizeBanner(jpgfile, outdir,size=(128,128)):
         print(e)
 
 if __name__ == '__main__':
-    num=len(glob('.\\wallpapers\\*.jpg'))
-    if num<=50:
-        genWallpapper()
-    genBanner()
+    imglist=glob('./wallpapers/*.jpg')
+    num=len(imglist)
+    genWallpapper(num)
+    genBanner(imglist)
